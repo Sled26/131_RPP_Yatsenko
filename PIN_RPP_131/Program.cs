0cc3f5edf67f1f59e8e2a29f8db704b5e178992e
Sled26 <Sledopyt@6mail.ru>
2016-02-05 12:12:17 +0300

    1 laba

---
+++ PIN_RPP_131/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 6;
+            int z = x + y;
+            Console.WriteLine(z);
+            if (z > 0)
+            {
+                Console.WriteLine("Больше нуля");
+            }
+            else
+            {
+                Console.WriteLine("Меньше нуля");
+            }
+            if (z % 2 == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Не четное");
+            }
+            if (z % 5 == 0)
+            {
+                Console.WriteLine("Делится на 5");
+            }
+            else
+            {
+                Console.WriteLine("Не делится на 5");
+            }
+            if (z % 10 == 0)
+            {
+                Console.WriteLine("Делится на 10");
+            }
+            else
+            {
+                Console.WriteLine("Не делится на 10");
+            }
+            if (z % 20 == 0)
+            {
+                Console.WriteLine("Делится на 20");
+            }
+            else
+            {
+                Console.WriteLine("Не делится на 20");
+            }
+        Start:
+            if (z >= 0)
+            {
+                Console.WriteLine(z--);
+                goto Start;
+            }
+
+            Console.Write(z = z + 10);
+        Start2:
+            if (z >= 0)
+            {
+                Console.WriteLine(--z);
+                goto Start2;
+            }
+            Console.Write(z = z + 10);
+            while (z >= 0)
+            {
+                Console.WriteLine(--z);
+            }
+            Console.Write(z = z + 10);
+            do
+            {
+                Console.WriteLine(z--);
+            } while (z >= 0);
+
+        }
+    }
+}
